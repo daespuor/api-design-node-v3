77e13ffb4768b71bff5d1ddb4da4ee8de47c0f99
Daniel <daespuor1991@gmail.com>
2020-07-06 21:36:00 -0500

    Mongo Models for item  with compund index

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,33 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      default: 'active',
+      required: true,
+      enum: ['active', 'complete', 'pastdue']
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'user'
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'list'
+    }
+  },
+  { timestamps: true }
+)
+itemSchema.index({ name: 1, list: -1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,25 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => {
+    res.send({ message: 'ok get' })
+  })
+  .post((req, res) => {
+    res.send({ message: 'ok post' })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send({ message: 'ok get' })
+  })
+  .put((req, res) => {
+    res.send({ message: 'ok put' })
+  })
+  .delete((req, res) => {
+    res.send({ message: 'ok delete' })
+  })
+
 export default router
